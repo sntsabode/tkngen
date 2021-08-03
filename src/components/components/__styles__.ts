a6176c55a0cac54c5b5f2f0195d92276f7d8088e
snts.abode <snts.abode@gmail.com>
2021-08-03 17:55:55 +0200

    styles

---
+++ src/components/components/__styles__.ts
@@ -0,0 +1,28 @@
+export const Styles = {
+  'modal': {
+    position: 'absolute',
+    width: '35%',
+    height: '60%',
+    border: 'none',
+    background: 'linear-gradient(to right bottom, rgba(255, 255, 255, 0.7), rgba(255, 255, 255, 0.3))',
+    borderRadius: '2rem',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+
+  'modal-btn': {
+    width: '40%',
+    height: '4rem',
+    color: 'gray !important',
+    border: 'none',
+    borderRadius: '2rem',
+    fontSize: '14pt',
+    fontWeight: 700
+  }
+}
+
+export const modalStyle = {
+  top: '20%',
+  left: '32.5%',
+}
