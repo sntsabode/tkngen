65fcd5176db2a1972e250c25ed4162f9f895d1f3
snts.abode <snts.abode@gmail.com>
2021-08-04 20:12:19 +0200

    `Loader` component

---
+++ src/components/components/utils/Loader.tsx
@@ -0,0 +1,21 @@
+import { makeStyles } from '@material-ui/core/styles'
+import LinearProgress from '@material-ui/core/LinearProgress'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    '& > * + *': {
+      marginTop: theme.spacing(2),
+    }
+  }
+}))
+
+export const Loader = () => {
+  const classes = useStyles()
+
+  return (
+    <div className={classes.root}>
+      <LinearProgress />
+    </div>
+  )
+}
